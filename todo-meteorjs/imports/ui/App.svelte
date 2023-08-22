479907d66eb6832ed251cf1af2c4e1868077c53f
Henrique Melo <hmelo2509@gmail.com>
2023-08-21 23:34:04 -0300

    chapter 6

--- todo-meteorjs/imports/ui/App.svelte
+++ todo-meteorjs/imports/ui/App.svelte
@@ -4,7 +4,28 @@
   import Task from './Task.svelte';
   import { TasksCollection } from '../api/TasksCollection';
 
-  $: tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 }}).fetch());
+  let hideCompleted = false;
+    
+  const setHideCompleted = value => hideCompleted = value;
+
+  let incompleteCount;
+  let pendingTasksTitle = '';
+  let tasks = [];
+
+  $: { 
+    tasks = useTracker(() =>  TasksCollection.find(
+      hideCompleted ? hideCompletedFilter : {}, { sort: { createdAt: -1 } }
+    ).fetch());
+
+    incompleteCount = TasksCollection.find(hideCompletedFilter).count();
+
+    pendingTasksTitle = `${
+            incompleteCount ? ` (${incompleteCount})` : ''
+    }`;
+  }
+
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+
 </script>
 
 
@@ -12,13 +33,19 @@
   <header>
       <div class="app-bar">
           <div class="app-header">
-              <h1>📝️ To Do List</h1>
+              <h1>📝️ To Do List {pendingTasksTitle}</h1>
           </div>
       </div>
   </header>
 
   <div class="main">
       <TaskForm />
+
+      <div class="filter">
+        <button on:click={() => setHideCompleted(!hideCompleted)}>
+            {hideCompleted ? 'Show All' : 'Hide Completed'}
+        </button>
+      </div>
 
       <ul class="tasks">
           {#each tasks as task (task._id)}
